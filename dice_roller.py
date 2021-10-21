cfcf3221a98763592d6c7ccc531dbabc53c30e1e
Naveen Gowda <naveen@foyernet.com>
2021-10-21 22:01:05 +0530

    tutorial complete

--- dice_roller.py
+++ dice_roller.py
@@ -1,12 +1,39 @@
 import random
-def main(min,max):
-  dice_rolls = 2
+def main():
+  dice_rolls = int(input('How many dice would you like to roll? '))
+  dice_size = int(input('How many sides are the dice? '))
   dice_sum = 0
   for i in range(0,dice_rolls):
-   roll = random.randint(1,6)
-  dice_sum += roll
-  print(f'You rolled a {roll}')
-  print(f'You have rolled a total of {dice_sum}')
+    roll = random.randint(1,dice_size)
+    dice_sum += roll
+  if roll == 1:
+    print(f'You rolled a {roll}! Critical Fail')
+  elif roll == dice_size:
+    print(f'You rolled a {roll}! Critical Success!')
+  else:
+    print(f'You rolled a {roll}')
+    print(f'You have rolled a total of {dice_sum}')
+
+  # dice_rolls = 2
+  # dice_sum = 0
+  # for i in range(0,dice_rolls):
+  #   roll = random.randint(1,6)
+  #   dice_sum += roll
+  # if roll == 1:
+  #   print(f'You rolled a {roll}! Critical Fail')
+  # elif roll == 6:
+  #   print(f'You rolled a {roll}! Critical Success!')
+  # else:
+  #   print(f'You rolled a {roll}')
+  # print(f'You have rolled a total of {dice_sum}')
+
+  # dice_rolls = 2
+  # dice_sum = 0
+  # for i in range(0,dice_rolls):
+  #  roll = random.randint(1,6)
+  # dice_sum += roll
+  # print(f'You rolled a {roll}')
+  # print(f'You have rolled a total of {dice_sum}')
   # while True:
 #  dice_rolls = 2
 #  for i in range(0,dice_rolls):
@@ -17,4 +44,4 @@ def main(min,max):
     # if choice.lower() == 'no':
         # break
 if __name__== "__main__":
-  main(1,6)
+  main()
