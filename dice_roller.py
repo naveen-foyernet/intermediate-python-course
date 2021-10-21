2396a6afc84080f057a0ea6548e19a6281b0c5b7
Naveen Gowda <naveen@foyernet.com>
2021-10-21 21:27:07 +0530

    now rolling two dice

--- dice_roller.py
+++ dice_roller.py
@@ -1,10 +1,20 @@
 import random
 def main(min,max):
-  while True:
-    print('You rolled a die')
-    print(f"your number  :{random.randint(min,max)}")
-    choice = input("Do you want roll the dice again? (yes/no)")
-    if choice.lower() == 'no':
-        break
+  dice_rolls = 2
+  dice_sum = 0
+  for i in range(0,dice_rolls):
+   roll = random.randint(1,6)
+  dice_sum += roll
+  print(f'You rolled a {roll}')
+  print(f'You have rolled a total of {dice_sum}')
+  # while True:
+#  dice_rolls = 2
+#  for i in range(0,dice_rolls):
+#   roll = random.randint(1,6)
+#  print(f'You rolled a {roll}')
+    # print(f"your number  :{random.randint(min,max)}")
+    # choice = input("Do you want roll the dice again? (yes/no)")
+    # if choice.lower() == 'no':
+        # break
 if __name__== "__main__":
   main(1,6)
